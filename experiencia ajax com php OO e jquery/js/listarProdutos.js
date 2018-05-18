054e3504cadab8ef490d9318243bcc9346a4ef9f
Alexandre Ximenes <xyymenes@gmail.com>
2018-05-18 10:32:27 -0300

    refatorando codigo para criar linha da table

--- experiencia ajax com php OO e jquery/js/listarProdutos.js
+++ experiencia ajax com php OO e jquery/js/listarProdutos.js
@@ -12,34 +12,37 @@ function buscarDadosNoServidor(){
     $.get(url, function(data){
         $("#table-produto").show();
         
-        data.map(d => {
-            linha = $('<tr>');
-            var tdId = $('<td>').addClass('produto-id').text(d.id);
-            var tdNome = $('<td>').text(d.nome);
-            var tdPreco = $('<td>').text(d.preco);
-            var colunaAcao = $('<td>');
-            var linkRemover = $('<a>').addClass('remover-produto').attr('href', '#').text('remover');
-            var linkEditar = $('<a>').addClass('editar-produto').attr('href', '#').text('editar');
-
-            colunaAcao.append(linkRemover);
-            colunaAcao.append(" | ");
-            colunaAcao.append(linkEditar);
-
-            linha.append(tdId);
-            linha.append(tdNome);
-            linha.append(tdPreco);
-            linha.append(colunaAcao);
-            
-            linha.find('.remover-produto').click(removeLinha);
-            linha.find(".editar-produto").click(editaLinha);
-
-            $("#table-produto tbody").append(linha);
+        data.map(produto => {
+            let tr = criaTr(produto);
+            $("#table-produto tbody").append(tr);
         });
         
     });
 }
 
+function criaTr(produto){
+    let linha = $('<tr>');
+        var tdId = $('<td>').addClass('produto-id').text(produto.id);
+        var tdNome = $("<td>").text(produto.nome);
+        var tdPreco = $("<td>").text(produto.preco);
+        var colunaAcao = $('<td>');
+        var linkRemover = $('<a>').addClass('remover-produto').attr('href', '#').text('remover');
+        var linkEditar = $('<a>').addClass('editar-produto').attr('href', '#').text('editar');
 
+        colunaAcao.append(linkRemover);
+        colunaAcao.append(" | ");
+        colunaAcao.append(linkEditar);
+
+        linha.append(tdId);
+        linha.append(tdNome);
+        linha.append(tdPreco);
+        linha.append(colunaAcao);
+
+        linha.find('.remover-produto').click(removeLinha);
+        linha.find(".editar-produto").click(editaLinha);
+    
+    return linha;
+}
 
 function editaLinha() {
     event.preventDefault();
